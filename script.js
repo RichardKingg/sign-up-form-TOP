ce2d6721d1296a79b88bd97fbd0c3e7b48bf6b8e
RichardKingg <ricardo.elizondo.jau@gmail.com>
2022-11-20 01:37:18 -0600

    Add function and event listener for styling validation for password inputs

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let confP = document.getElementById("confP");
 let passStr = "";
 let passConfStr = "";
 
+//Stores the variable from password input field
 passField.addEventListener("keydown", (event) => {
   if (event.key === "Backspace") {
     return (passStr = passStr.slice(0, -1));
@@ -26,6 +27,7 @@ passField.addEventListener("keydown", (event) => {
   return (passStr += event.key);
 });
 
+//Stores the variable from confirm password input field
 passConfField.addEventListener("keydown", (event) => {
   if (event.key === "Backspace") {
     return (passConfStr = passConfStr.slice(0, -1));
@@ -45,4 +47,24 @@ passConfField.addEventListener("keydown", (event) => {
   }
 
   return (passConfStr += event.key);
+});
+
+//Initial default state with style for border and span
+if (passConfStr === "" || passStr !== passConfStr) {
+  confP.textContent = "✖";
+  confP.style.color = "#79008e";
+  passConfField.style.borderColor = "#79008e";
+}
+
+//Re evaluates everytime you finish typing and styles border and span
+passConfField.addEventListener("keyup", () => {
+  if (passConfStr === "" || passStr !== passConfStr) {
+    confP.textContent = "✖";
+    confP.style.color = "#79008e";
+    passConfField.style.borderColor = "#79008e";
+  } else if (passStr === passConfStr) {
+    confP.textContent = "✓";
+    confP.style.color = "#07008d";
+    passConfField.style.borderColor = "#07008d";
+  }
 });
